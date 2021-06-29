3c9ad55f634b2d91effff9a5d009f06ca1ce4773
零式因子 <294594605@qq.com>
2021-06-29 15:33:10 +0800

    配置txt

--- LR/skleran.py
+++ LR/skleran.py
@@ -15,35 +15,44 @@ from sklearn.metrics import confusion_matrix
 # 支持中文格式
 from pylab import mpl
 
-labels = ['0', '1']
-
-
-def plot_confusion_matrix(cm, title='Confusion Matrix', cmap=plt.cm.binary):
+def plot_confusion_matrix(y,y_pred,cmap=plt.cm.Blues,title='混淆矩阵'):
     """
     绘制混淆矩阵
+    :param y: 真实值
+    :param y_pred: 预测值
     :param cm: 混淆矩阵
     :param title: 图像标题
     :param cmap: 热力图的颜色
     :return:
     """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
+    cm = confusion_matrix(y, y_pred)
+    classes = list(set(y))
+    classes.sort()
+    plt.imshow(cm, cmap)
+    indices = range(len(cm))
+    plt.xticks(indices, classes)
+    plt.yticks(indices, classes)
+    # 热度显示仪
     plt.colorbar()
-    xlocations = np.array(range(len(labels)))
-    plt.xticks(xlocations, labels, rotation=90)
-    plt.yticks(xlocations, labels)
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    # 就是坐标轴含义说明了
+    plt.xlabel('guess')
+    plt.ylabel('fact')
+    plt.title('训练集混淆矩阵')
+    # 显示数据，直观些
+    for first_index in range(len(cm)):
+        for second_index in range(len(cm[first_index])):
+            plt.text(first_index, second_index, cm[first_index][second_index])
+    # 显示
 
 
-def create_meshgrid_pic(plt, predict, X, Y, step=0.01):
+def create_meshgrid_pic(plt,predict, X, Y, step=0.01):
     """
     画分类网格
     :param plt: 画图对象
     :param predict: 预测对象
-    :param X:
-    :param Y:
-    :param step:
+    :param X:特征1
+    :param Y:特征2
+    :param step:图像跨度
     :return:
     """
     # 确认训练集的边界
@@ -66,21 +75,22 @@ def create_meshgrid_pic(plt, predict, X, Y, step=0.01):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)  # 等位线
 
 
-def sandiantu(plt, X, y):
+def sandiantu(plt, X, y,title='散点图'):
     """
     画目标标签为二分类的二维散点图
     :param plt: 画图对象
-    :param X:
-    :param Y:
-    :param step:
+    :param title: 图像标题
+    :param X:数据集
+    :param y:结果
     :return:
     """
     plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='o', label='P')  # 前50个样本的散点图
     plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='x', label='NP')  # 中间50个样本的散点图
-    plt.xlabel('Age length')
-    plt.ylabel('EstimatedSalary length')
+    plt.xlabel('Age')
+    plt.ylabel('EstimatedSalary')
     # 把说明放在左上角，具体请参考官方文档
     plt.legend(loc=2)
+    plt.title(title)
 
 
 if __name__ == '__main__':
@@ -91,11 +101,13 @@ if __name__ == '__main__':
     # 加载数据
     df = pd.read_csv('./data/Purchasing Desire.csv', header=0)
 
+
     # 取其中两个特征
     X = df[['Age', 'EstimatedSalary']].values
     y = df.loc[:, 'Purchased'].values
     #  print(X[y==0,0])
     #  print(X[y==0,1])
+    labels =list(set(y))
 
     # 画散点图
     sandiantu(plt, X, y)
@@ -136,31 +148,16 @@ if __name__ == '__main__':
     # 训练集画散点图
     plt.clf()
     create_meshgrid_pic(plt, lr.predict, X_train_std[:, 0], X_train_std[:, 1])
-    sandiantu(plt, X_train_std, y_train)
+    sandiantu(plt, X_train_std, y_train,title='训练集散点图',)
     plt.show()
     plt.close()
 
     # 训练集画混淆矩阵
     y_train_pred = lr.predict(X_train_std)
-    cm = confusion_matrix(y_train, y_train_pred)
-    classes = list(set(y_train))
-    classes.sort()
-    plt.imshow(cm, cmap=plt.cm.Blues)
-    indices = range(len(cm))
-    plt.xticks(indices, classes)
-    plt.yticks(indices, classes)
-    # 热度显示仪
-    plt.colorbar()
-    # 就是坐标轴含义说明了
-    plt.xlabel('guess')
-    plt.ylabel('fact')
-    plt.title('训练集混淆矩阵')
-    # 显示数据，直观些
-    for first_index in range(len(cm)):
-        for second_index in range(len(cm[first_index])):
-            plt.text(first_index, second_index, cm[first_index][second_index])
-    # 显示
+    plot_confusion_matrix(y_train, y_train_pred, title='训练集混淆矩阵')
     plt.show()
+    plt.close()
+
 
     # 测试集 以下同理训练集
     test_right_counts = 0
@@ -173,4 +170,24 @@ if __name__ == '__main__':
 
     print("测试集准确率：", ((test_right_counts * 1.0) / X_test_std.shape[0]))
     plt.close('all')
-    # # 化边界函数
+
+    # 测试集画散点图
+    plt.clf()
+    create_meshgrid_pic(plt,
+                        lr.predict,
+                        X_test_std[:, 0],
+                        X_test_std[:, 1])
+    sandiantu(plt,
+              X_test_std,
+              y_test,
+              title='测试集散点图')
+    plt.show()
+    plt.close()
+
+    # 测试集画混淆矩阵
+    y_test_pred = lr.predict(X_test_std)
+    plot_confusion_matrix(y_test,
+                          y_test_pred,
+                          title='测试集混淆矩阵')
+    plt.show()
+    plt.close()
