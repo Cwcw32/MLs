d5cb5271020a45800e6551cb4d8c23ad69174940
零式因子 <294594605@qq.com>
2021-06-29 15:54:06 +0800

    1稿完成

--- LR/skleran.py
+++ LR/skleran.py
@@ -15,14 +15,14 @@ from sklearn.metrics import confusion_matrix
 # 支持中文格式
 from pylab import mpl
 
-def plot_confusion_matrix(y,y_pred,cmap=plt.cm.Blues,title='混淆矩阵'):
+
+def plot_confusion_matrix(y, y_pred, cmap=plt.cm.Blues, title='混淆矩阵'):
     """
     绘制混淆矩阵
     :param y: 真实值
     :param y_pred: 预测值
-    :param cm: 混淆矩阵
-    :param title: 图像标题
     :param cmap: 热力图的颜色
+    :param title: 图像标题
     :return:
     """
     cm = confusion_matrix(y, y_pred)
@@ -37,7 +37,7 @@ def plot_confusion_matrix(y,y_pred,cmap=plt.cm.Blues,title='混淆矩阵'):
     # 就是坐标轴含义说明了
     plt.xlabel('guess')
     plt.ylabel('fact')
-    plt.title('训练集混淆矩阵')
+    plt.title(title)
     # 显示数据，直观些
     for first_index in range(len(cm)):
         for second_index in range(len(cm[first_index])):
@@ -45,7 +45,7 @@ def plot_confusion_matrix(y,y_pred,cmap=plt.cm.Blues,title='混淆矩阵'):
     # 显示
 
 
-def create_meshgrid_pic(plt,predict, X, Y, step=0.01):
+def create_meshgrid_pic(plt, predict, X, Y, step=0.01):
     """
     画分类网格
     :param plt: 画图对象
@@ -75,7 +75,7 @@ def create_meshgrid_pic(plt,predict, X, Y, step=0.01):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)  # 等位线
 
 
-def sandiantu(plt, X, y,title='散点图'):
+def sandiantu(plt, X, y, title='散点图'):
     """
     画目标标签为二分类的二维散点图
     :param plt: 画图对象
@@ -84,8 +84,11 @@ def sandiantu(plt, X, y,title='散点图'):
     :param y:结果
     :return:
     """
-    plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='o', label='P')  # 前50个样本的散点图
-    plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='x', label='NP')  # 中间50个样本的散点图
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], s=10,color='red', marker='o', label='0')
+    print(X[y == 0, 0].size)
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], s=10,color='blue', marker='x', label='1')
+    print(X[y == 1, 0].size)
+
     plt.xlabel('Age')
     plt.ylabel('EstimatedSalary')
     # 把说明放在左上角，具体请参考官方文档
@@ -101,13 +104,12 @@ if __name__ == '__main__':
     # 加载数据
     df = pd.read_csv('./data/Purchasing Desire.csv', header=0)
 
-
     # 取其中两个特征
     X = df[['Age', 'EstimatedSalary']].values
     y = df.loc[:, 'Purchased'].values
     #  print(X[y==0,0])
     #  print(X[y==0,1])
-    labels =list(set(y))
+    labels = list(set(y))
 
     # 画散点图
     sandiantu(plt, X, y)
@@ -135,6 +137,7 @@ if __name__ == '__main__':
     # print(y_train)
 
     '-----------------训练集结果-----------------'
+    print(X_train_std.shape)
     train_right_counts = 0
     for i in range(X_train_std.shape[0]):
         # print(i)
@@ -148,7 +151,7 @@ if __name__ == '__main__':
     # 训练集画散点图
     plt.clf()
     create_meshgrid_pic(plt, lr.predict, X_train_std[:, 0], X_train_std[:, 1])
-    sandiantu(plt, X_train_std, y_train,title='训练集散点图',)
+    sandiantu(plt, X_train_std, y_train, title='训练集散点图', )
     plt.show()
     plt.close()
 
@@ -158,8 +161,8 @@ if __name__ == '__main__':
     plt.show()
     plt.close()
 
-
     # 测试集 以下同理训练集
+    print(X_test_std.shape)
     test_right_counts = 0
     for i in range(X_test_std.shape[0]):
         original_val = y_test[i]
@@ -185,6 +188,7 @@ if __name__ == '__main__':
     plt.close()
 
     # 测试集画混淆矩阵
+    lr.fit(X_test_std, y_test)
     y_test_pred = lr.predict(X_test_std)
     plot_confusion_matrix(y_test,
                           y_test_pred,
